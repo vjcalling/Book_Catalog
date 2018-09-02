12f8c9d92bf318b354a35e8a6ec890ace3cb2549
vjcalling <vibhor.jain@outlook.com>
2018-09-02 15:48:09 +0530

    code added for delete/edit book

--- app/catalog/routes.py
+++ app/catalog/routes.py
@@ -1,7 +1,10 @@
 from app.catalog import main
 from app import db
 from app.catalog.models import Book, Publication
-from flask import render_template
+from flask import render_template, flash, request, redirect, url_for
+from flask_login import login_required
+from app.catalog.forms import EditBookForm, CreateBookForm
+
 
 @main.route('/')
 def display_books():
@@ -15,3 +18,32 @@ def display_publisher(publisher_id):
     return render_template('publisher.html',
                            publisher=publisher,
                            publisher_books=publisher_books)
+
+
+@main.route('/book/delete/<book_id>', methods=['GET', 'POST'])
+@login_required
+def delete_book(book_id):
+    book = Book.query.get(book_id)
+    if request.method == 'POST':
+        db.session.delete(book)
+        db.session.commit()
+        flash('Book Deleted Successfully')
+        return redirect(url_for('main.display_books'))
+    return render_template('delete_book.html', book=book, book_id=book.id)
+
+
+
+@main.route('/edit/book/<book_id>', methods=['GET', 'POST'])
+@login_required
+def edit_book(book_id):
+    book = Book.query.get(book_id)
+    form = EditBookForm(obj=book)
+    if form.validate_on_submit():
+        book.title = form.title.data
+        book.format = form.format.data
+        book.num_pages = form.num_pages.data
+        db.session.add(book)
+        db.session.commit()
+        flash('Book Edited Successfully')
+        return redirect(url_for('main.display_books'))
+    return render_template('edit_book.html', form=form)
