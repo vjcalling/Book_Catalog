747836528949a5b550f30425995b6487d41a4579
vjcalling <vibhor.jain@outlook.com>
2018-09-02 10:44:14 +0530

    adding models for Book and Publication

--- app/catalog/routes.py
+++ app/catalog/routes.py
@@ -1,7 +1,10 @@
 from app.catalog import main
-
+from app import db
+from app.catalog.models import Book, Publication
+from flask import render_template
 
 @main.route('/')
-def hello():
-    return 'hello world'
+def display_book():
+    books = Book.query.all()
+    return render_template('home.html', books=books)
 
--- run.py
+++ run.py
@@ -1,6 +1,8 @@
-from app import create_app
+from app import create_app, db
 
 if __name__ == '__main__':
     flask_app = create_app('dev')
+    with flask_app.app_context():
+        db.create_all()
     flask_app.run()
 
