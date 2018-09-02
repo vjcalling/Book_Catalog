585a9077d268999c0f26cd4cd53dc376695fd2d1
vjcalling <vibhor.jain@outlook.com>
2018-09-02 11:26:03 +0530

    adding layout

--- app/catalog/routes.py
+++ app/catalog/routes.py
@@ -4,7 +4,14 @@ from app.catalog.models import Book, Publication
 from flask import render_template
 
 @main.route('/')
-def display_book():
+def display_books():
     books = Book.query.all()
     return render_template('home.html', books=books)
 
+@main.route('/display/publisher/<publisher_id>')
+def display_publisher(publisher_id):
+    publisher = Publication.query.filter_by(id=publisher_id).first()
+    publisher_books = Book.query.filter_by(pub_id=publisher.id).all()
+    return render_template('publisher.html',
+                           publisher=publisher,
+                           publisher_books=publisher_books)
